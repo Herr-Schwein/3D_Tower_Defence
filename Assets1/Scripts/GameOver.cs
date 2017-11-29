26e1d3d0685aa394a9ead97d0ed8e6388b60a49a
Herr Schwein <davidtaffyshaking@hotmail.com>
2017-11-29 18:10:28 -0500

    merge version

---
+++ Assets1/Scripts/GameOver.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	public Text roundText;
+	public string menuSceneName = "MainMenu";
+
+	void OnEnable () {
+		roundText.text = PlayerStats.Rounds.ToString();
+	}
+
+	public void Retry () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		WaveSpawner.EnemiesAlive = 0;
+	}
+
+	public void Menu () {
+		SceneManager.LoadScene(menuSceneName);
+	}
+}
